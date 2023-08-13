012f63dd0ebc4be4d4b39bd0d8c5e8a4287d166c
moali <mohammedali01071@gmail.com>
2023-08-13 19:39:21 +0300

    done

--- firstline.go
+++ firstline.go
@@ -1,19 +1,8 @@
 package ascii
 
-import "fmt"
-
-// import "fmt"
-
-func FirstLine(char rune) (firstLine int, err string) {
-	// var firstLine int
-	if char >= 32 && char <= 126 {
-		diff := int(char) - 32
-		firstLine = diff*9 + 2
-	} else {
-		err = "ERROR: Character out of range"
-		fmt.Println(err)
-	}
-	return firstLine, err
+func FirstLine(char rune) int {
+	var firstLine int
+	diff := int(char) - 32
+	firstLine = diff*9 + 2
+	return firstLine
 }
-
-// move error handling to main
--- main/main.go
+++ main/main.go
@@ -1,29 +1,73 @@
 package main
 
 import (
-	// "ascii"
 	"ascii"
 	"fmt"
-	"log"
 	"os"
+	"strings"
 )
 
-func main() {
-	file, err := os.Open("standard.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+// func main() {
+// 	file, err := os.Open("standard.txt")
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	defer file.Close()
 
+// 	scanner := bufio.NewScanner(file)
+// 	line := 1
+// 	var firstLine int
+// 	for scanner.Scan() {
+// 		str := os.Args[1]
+// 		for _, char := range str {
+// 			firstLine = ascii.FirstLine(char)
+// 			if scanner.Text() == "\n" {
+// 				line++
+// 			}
+// 			if line == firstLine {
+// 				fmt.Print(scanner.Text())
+// 			}
+// 		}
+// 	}
+// 	fmt.Println("first line = ", firstLine)
+
+// 	// str := os.Args[1]
+// 	// for _, char := range str {
+// 	// 	firstLine, _ := ascii.FirstLine(char)
+// 	// fmt.Println(firstLine)
+// 	// lastLine := firstLine + 8 // || firstline + lineCount
+// }
+
+func main() {
 	str := os.Args[1]
+	var firstLines []int
 	for _, char := range str {
-		firstLine, _ := ascii.FirstLine(char)
-		fmt.Println(firstLine)
-		// lastLine := firstLine + 8 // || firstline + lineCount
+		if char < 32 || char > 126 {
+			fmt.Println("ERROR: Character out of range")
+			return
+		} else {
+			firstLine := ascii.FirstLine(char)
+			firstLines = append(firstLines, firstLine)
+		}
+	}
+	// Open the file in read mode.
+	file, err := os.ReadFile("standard.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Split the data into a slice of strings, one for each line.
+	lines := strings.Split(string(file), "\n")
+	// fmt.Println(lines)
+
+	// Get the lines from 11 to 19.
+	for i := 1; i < 8; i++ {
+		for i, line := range firstLines {
+			filteredLines := lines[line-1]
+			fmt.Print(filteredLines)
+			firstLines[i]++
+		}
+		fmt.Println()
 	}
 }
-
-// create an offset function: if char == 32, nil offset; for a unit increase in char, offset is 8 ...
-// firstLine = var*8 ...
-// var = char - 32
-// if char == 32, firstLine = 1
