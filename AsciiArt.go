73328594f58da9680edccd1c018aed72942a2c2e
moali <mohammedali01071@gmail.com>
2023-08-22 21:12:18 +0300

    done :)

--- AsciiArt.go
+++ AsciiArt.go
@@ -11,36 +11,43 @@ func AsciiArt() {
 		fmt.Println("ERROR: no arguments to print")
 		return
 	}
-	var args string
-	for i, arg := range os.Args {
-		if i >= 1 {
-			args += arg + " "
+	// var args string
+	// for i, arg := range os.Args {
+	// 	if i >= 1 {
+	// 		args += arg + " "
+	// 	}
+	// }
+	
+	phrases := strings.Split(os.Args[1], "\\n")
+	fmt.Println(phrases)
+		
+	for _, phrase := range phrases{
+		var firstLines []int
+		for _, char := range phrase {
+			if char < 32 || char > 126 {
+				fmt.Println("ERROR: Character out of range")
+				return
+			} else {
+				firstLine := FirstLine(char)
+				firstLines = append(firstLines, firstLine)
+			}
 		}
-	}
-	var firstLines []int
-	for _, char := range args {
-		if char < 32 || char > 126 {
-			fmt.Println("ERROR: Character out of range")
+
+		// Read the file
+		file, err := os.ReadFile("standard.txt")
+		if err != nil {
+			fmt.Println(err)
 			return
-		} else {
-			firstLine := FirstLine(char)
-			firstLines = append(firstLines, firstLine)
 		}
-	}
-	// Read the file
-	file, err := os.ReadFile("standard.txt")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	// Split the data into a slice of strings, one for each line.
-	lines := strings.Split(string(file), "\n")
-	for i := 1; i < 9; i++ {
-		for i, line := range firstLines {
-			filteredLines := lines[line-1]
-			fmt.Print(filteredLines)
-			firstLines[i]++
+		// Split the data into a slice of strings, one for each line.
+		lines := strings.Split(string(file), "\n")
+		for i := 1; i < 9; i++ {
+			for j, line := range firstLines {
+				filteredLines := lines[line-1]
+				fmt.Print(filteredLines)
+				firstLines[j]++
+			}
+			fmt.Println()
 		}
-		fmt.Println()
 	}
 }
