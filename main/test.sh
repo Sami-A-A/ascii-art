c5ac9d92829eaaaede67876cc111675c61b34ba2
Sami AbdulAal <sami.abdulaal@gmail.com>
2023-09-01 05:03:27 +0300

    test cases changed

--- main/test.sh
+++ main/test.sh
@@ -3,16 +3,14 @@ go run . "" | cat -e
 echo "Test 2:"
 go run . "\n" | cat -e
 echo "Test 3:"
-go run . "Hello\n" | cat -e
+go run . "\n\n" | cat -e
 echo "Test 4:"
- go run . "hello" | cat -e
+ go run . "\nhello" | cat -e
 echo "Test 5:"
- go run . "HeLlO" | cat -e
+ go run . "\nhello\n" | cat -e
 echo "Test 6:"
- go run . "Hello There" | cat -e
+ go run . "hello\n" | cat -e
 echo "Test 7:"
- go run . "{Hello There}" | cat -e
-echo "Test 8:"
  go run . "Hello\nThere" | cat -e
-echo "Test 9:"
+echo "Test 8:"
  go run . "Hello\n\nThere" | cat -e
