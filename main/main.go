aa588574ab9fd284ebd12b7c92ef4437f0edbc70
moali <mohammedali01071@gmail.com>
2023-09-04 19:13:21 +0300

    ascii-art

--- main/main.go
+++ main/main.go
@@ -2,31 +2,18 @@ package main
 
 import (
 	ascii "ascii/pkg"
-	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
-	// Handle error if no arguments are passed
-	if len(os.Args) < 2 {
-		fmt.Println("ERROR: Insufficient arguments")
-		return
-	}
-
-	// Create output flag and return output file name
-	fileName, err := ascii.OutputFlag()
+	// Handle args error
+	err := ascii.ArgsErrors()
 	if err {
 		return
 	}
 
-	// Error handling; flag arguments: should be a single arg
-	err = ascii.FlagArgErrors()
-	if err {
-		return
-	}
-
-	// Convert input to art and print output to desired file
-	art := ascii.AsciiArt(flag.Args()[0])
-	ascii.PrintOutput(art, fileName)
+	// Convert input to art and print result
+	art := ascii.AsciiArt(os.Args[1])
+	fmt.Print(art)
 }
